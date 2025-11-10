b0b4f3c274f8a3b7e917898b0db6334870905deb
IzzetGunturk <izzetgunturk2@hotmail.com>
2025-11-10 20:31:45 +0100

    task/api Information from API is visible in HTML

--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -1,20 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { ApiResponse ,Timings } from '../../interfaces/prayertimes.interfaces';       
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-application',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './application.component.html',
   styleUrl: './application.component.scss'
 })
 export class ApplicationComponent {
 
+  prayerTimes?: Timings;
+
   private apiService = inject(ApiService);
 
   getApiInformation() {
     this.apiService.getPrayerTimes('Roermond', 'Netherlands').subscribe({
-      next: (response) => {
+      next: (response: ApiResponse) => {
+        this.prayerTimes = response.data.timings;
         console.log('Gebedstijden:', response.data.timings);
         console.log(response.data)
       },
@@ -22,9 +27,5 @@ export class ApplicationComponent {
         console.error('Fout bij het ophalen:', err);
       }
     });
-  }
-
-  ngOnInit() {
-    this.getApiInformation();
   }
 }
