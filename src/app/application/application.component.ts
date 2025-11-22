2dc6e0cba6d794a93430c06c4f3b0b623a086e38
IzzetGunturk <izzetgunturk2@hotmail.com>
2025-11-22 23:23:20 +0100

    task/time-and-date Added getNextPrayer and timeLeftPrayer functions

--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -14,6 +14,8 @@ import { TimeAndDateComponent } from '../time-and-date/time-and-date.component';
 export class ApplicationComponent {
 
   prayerTimes?: Timings;
+  prayerOrder = ['Fajr','Sunrise','Dhuhr','Asr','Maghrib','Isha'];
+  timeLeft?: string;
 
   private apiService = inject(ApiService);
 
@@ -21,8 +23,7 @@ export class ApplicationComponent {
     this.apiService.getPrayerTimes('Roermond', 'Netherlands').subscribe({
       next: (response: ApiResponse) => {
         this.prayerTimes = response.data.timings;
-        console.log('Gebedstijden:', response.data.timings);
-        console.log(response.data)
+        this.timeLeftPrayer();
       },
       error: (err) => {
         console.error('Fout bij het ophalen:', err);
@@ -30,7 +31,51 @@ export class ApplicationComponent {
     });
   }
 
+  getNextPrayer(): { name: string; date: Date } | null {
+    if (!this.prayerTimes) return null;
+
+    const dateNow = new Date();
+
+    for (const prayer of this.prayerOrder) {
+      const [h, m] = this.prayerTimes[prayer as keyof Timings].split(':').map(Number);
+      const prayerDate = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate(), h, m);
+
+      if (prayerDate > dateNow) {
+        return { name: prayer, date: prayerDate };
+      }
+    }
+
+    const [fh, fm] = this.prayerTimes.Fajr.split(':').map(Number);
+    const fajrTomorrow = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + 1, fh, fm);
+
+
+    return { name: 'Fajr', date: fajrTomorrow };
+  }
+
+  timeLeftPrayer() {
+    const dateNow = new Date();
+    const nextPrayer = this.getNextPrayer();
+
+    if (!nextPrayer) return;
+
+    const differenceTime = nextPrayer.date.getTime() - dateNow.getTime();
+
+    const hours = Math.floor(differenceTime / (1000 * 60 * 60));
+    const minutes = Math.floor((differenceTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((differenceTime % (1000 * 60)) / 1000);
+
+    const [hStr, mStr, sStr] = [hours, minutes, seconds].map(
+      timeDigits => timeDigits.toString().padStart(2, '0')
+    );
+
+    this.timeLeft = `Prayer: ${nextPrayer.name} - ${hStr}:${mStr}:${sStr}`;
+  }
+
   ngOnInit() {
     this.getApiInformation();
+
+    setInterval(() => {
+      this.timeLeftPrayer();
+    }, 1000);
   }
 }
--- src/app/time-and-date/time-and-date.component.ts
+++ src/app/time-and-date/time-and-date.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class TimeAndDateComponent {
   dateNow = new Date();
-  timeNow: string = '';
+  timeNow = '';
   
   dateToday = new Intl.DateTimeFormat('nl-NL', {
     day: 'numeric',
